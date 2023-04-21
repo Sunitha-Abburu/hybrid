9d42c0542afc133a9647537033cdef8ef7d8c265
Mahesh <maheshjha9619@gmail.com>
2023-04-21 19:28:42 +0530

    adding

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-openai_key = "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"
+openai_key = "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"
 api_key = "fc6ea2f8-0ceb-4853-9919-e53444f84366"
